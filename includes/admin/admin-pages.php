6137d1d7a99d3f80d086a59c71a58beb0df52730
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-03-06 14:44:09 +0000

    include basic bootstrap of the component

---
+++ includes/admin/admin-pages.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Handles manipulation and registration of admin pages.
+ *
+ * @package     posterno-schema
+ * @copyright   Copyright (c) 2018, Pressmodo, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+namespace PNO\Schema;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Register admin pages for the plugin.
+ *
+ * @return void
+ */
+function admin_pages() {
+
+	add_submenu_page( 'edit.php?post_type=listings', esc_html__( 'Listings Schema Settings' ), __( 'Schema' ), 'manage_options', 'posterno-listings-schema', __NAMESPACE__ . '\\admin_listings_schema_page' );
+
+}
+add_action( 'admin_menu', __NAMESPACE__ . '\\admin_pages', 10 );
+
+/**
+ * Handles loading of the listings schema page.
+ * Actual output is handled via Vuejs.
+ *
+ * @return void
+ */
+function admin_listings_schema_page() {
+
+	echo '<div id="pno-listings-schema"></div>';
+
+}

7b92cd3721d53201cdae0406a04c71ba05c4cb15
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-03-06 14:32:52 +0000

    init

---
+++ plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name:     PosternoSchema
+ * Plugin URI:      #
+ * Description:     #
+ * Author:          #
+ * Author URI:      #
+ * Text Domain:     posterno-schema
+ * Domain Path:     #
+ * Version:         0.1.0
+ *
+ * PosternoSchema is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * PosternoSchema is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PosternoSchema. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package posterno-schema
+ * @author #
+ */
+
+defined( 'ABSPATH' ) || exit;

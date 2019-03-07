e8b63ef0f60dcb1feea7ddaf9801297d3321114e
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-03-07 10:56:04 +0000

    change vue entry app and port

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	filenameHashing: false,
+	// see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+	chainWebpack: config => {
+		// If you wish to remove the standard entry point
+		config.entryPoints.delete('app')
+		// then add your own
+		config
+			.entry('listings-schema-editor')
+			.add('./src/listings-schema-editor.js')
+			.end()
+	}
+}

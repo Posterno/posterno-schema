f87aa600ffa1f6cb315f926a5eb986c2fce14026
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-03-13 16:34:26 +0100

    remove tree select component

--- src/listings-schema-editor.js
+++ src/listings-schema-editor.js
@@ -3,11 +3,8 @@ import App from './listings-schema-editor.vue'
 import VueWP from '@alessandrotesoro/vuewp'
 import router from './listings-schema-router'
 import Select2 from 'v-select2-component'
-import Treeselect from '@riophae/vue-treeselect'
-import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 
 Vue.component('Select2', Select2);
-Vue.component('Treeselect', Treeselect);
 
 Vue.use(VueWP)
 

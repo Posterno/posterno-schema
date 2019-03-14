e776128dd3a6ad494d8f66c82d947871ed30416f
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-03-14 17:55:16 +0100

    load global schemas as an array

--- includes/Tag/Listing.php
+++ includes/Tag/Listing.php
@@ -9,6 +9,8 @@
 
 namespace PNO\SchemaOrg\Tag;
 
+use WP_Query;
+
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
@@ -39,6 +41,55 @@ class Listing {
 
 	}
 
+	private function get_global_schemas() {
+
+		$schemas = [];
+
+		$args = [
+			'post_type'              => 'pno_schema',
+			'posts_per_page'         => 100,
+			'nopaging'               => true,
+			'no_found_rows'          => true,
+			'update_post_term_cache' => false,
+			'post_status'            => 'publish',
+			'suppress_filters'       => true,
+			'fields'                 => 'ids',
+			'meta_query'             => array(
+				array(
+					'key'   => 'schema_mode',
+					'value' => 'global',
+				),
+			),
+
+		];
+
+		$query = new WP_Query( $args );
+
+		if ( $query->have_posts() ) {
+
+			while ( $query->have_posts() ) {
+
+				$query->the_post();
+
+				$post_id    = get_the_id();
+				$properties = get_post_meta( $post_id, 'schema_properties', true );
+
+				if ( is_array( $properties ) && ! empty( $properties ) ) {
+					$schemas[] = [
+						'post_id'    => $post_id,
+						'schema_id'  => get_post_meta( $post_id, 'schema_name', true ),
+						'properties' => get_post_meta( $post_id, 'schema_properties', true ),
+					];
+				}
+			}
+		}
+
+		wp_reset_postdata();
+
+		return $schemas;
+
+	}
+
 	/**
 	 * Load the structured data tag within the footer.
 	 *
@@ -49,6 +100,10 @@ class Listing {
 		if ( ! $this->should_load() ) {
 			return;
 		}
+
+		$global_schemas = $this->get_global_schemas();
+
+		print_r( $global_schemas );
 
 	}
 
